14bd4e079caeed80fcb5187fca1cf6158e8e261e
Dennis Gega <146154938+Dennis-Gega@users.noreply.github.com>
2024-06-22 13:27:55 -0400

    Get color scheme button is now functional, color scheme is displayed

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const colorPicker = document.getElementById('color-picker')
 const colorSchemeSelector = document.getElementById('color-scheme-selector')
 const getColorButton = document.getElementById('get-color-btn')
+const colorsContainer = document.getElementById('colors-container')
 
 getColorButton.addEventListener('click', () => {
-    getColorScheme()
-
+    handleClick()
 })
 
-function getColorScheme () {
-    
+function handleClick () {
+    // Fetch user input color and scheme, then fetch palette from ColorAPI and render palette on DOM
+
     const hexNumber = colorPicker.value.substring(1) // ColorAPI query cannot include '#' character
-    const colorScheme = colorSchemeSelector.value
+    const colorScheme = colorSchemeSelector.value.toLowerCase() // ColorAPI query needs color scheme to be fully lowercase
+    let html = ''
 
-    console.log(`https://www.thecolorapi.com/scheme?hex=${hexNumber}&value=${colorScheme}`)
-
-    fetch(`https://www.thecolorapi.com/scheme?hex=${hexNumber}&value=${colorScheme}`)
+    console.log(`https://www.thecolorapi.com/scheme?hex=${hexNumber}&mode=${colorScheme}`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexNumber}&mode=${colorScheme}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => data.colors)
+        .then(colors => {
+            for (let color of colors) {
+                html += `
+                    <div class="color-hex-container">
+                        <div class="color" style="background-color:${color.hex.value};"></div>
+                        <p>${color.hex.value}</p>
+                    </div>
+                `
+            }
+            colorsContainer.innerHTML = html
+        })
 }
